4eae2c4dda26b82eb6bb1b46e35c3f3e820fc029
Vitaly <vitalik.yakhimovich@mail.ru>
2023-04-10 23:40:02 +0300

    add Example 002

---
+++ Example002/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите программу,
+// которая выведет все четные натуральные числа в промежутке от m до N . Выполнить с помощью рекурсии.
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Count(int m, int n)
+{
+    if (m > n)return;
+
+    if (m % 2 == 0){Console.Write($"{m}, ");}
+
+    Count(m+1,n);
+
+}
+
+Count(m,n);
